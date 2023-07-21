5424846692bbc7b98d3277ff9996b43f4202862d
DevPranay-salesforce <pranayshelke95.sfdc@gmail.com>
2023-07-21 18:35:31 +0530

    mpik

--- src/test/java/PDFcomparision.java
+++ src/test/java/PDFcomparision.java
@@ -16,7 +16,7 @@ public class PDFcomparision {
 	 boolean isEquals = new PdfComparator("PDFcomparision11\\com11.pdf","PDFcomparision11\\com222.pdf").withIgnore("Ignore.conf").compare().writeTo("diffOutput");
 	 if (!isEquals) {
 		 System.out.println("Is PDF is same :" +isEquals);   
-	     System.out.println("Differences found!!!!!!22!!!!!");
+	     System.out.println("Differences found!!!!!!2222!!!!!");
 	 }
 }
 }
