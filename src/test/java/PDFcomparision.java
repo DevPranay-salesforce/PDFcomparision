d5592120b0386a00269f66ef8537d12f37cf4494
DevPranay-salesforce <pranayshelke95.sfdc@gmail.com>
2023-07-21 18:39:17 +0530

    dfgds

--- src/test/java/PDFcomparision.java
+++ src/test/java/PDFcomparision.java
@@ -16,7 +16,7 @@ public class PDFcomparision {
 	 boolean isEquals = new PdfComparator("PDFcomparision11\\com11.pdf","PDFcomparision11\\com222.pdf").withIgnore("Ignore.conf").compare().writeTo("diffOutput");
 	 if (!isEquals) {
 		 System.out.println("Is PDF is same :" +isEquals);   
-	     System.out.println("Differences found!!!!!!2222!!!!!");
+	     System.out.println("Differences found!!!!!!221122!!!!!");
 	 }
 }
 }
